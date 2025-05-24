6691b026e26155e6297be90e4f0e2bf9275f5cb7
Kanaru Nakahira <kanaru2917@gmail.com>
2025-05-25 00:17:55 +0900

    Add Dockerfile to frontend, and Dockerfile and requirements.txt to backend for Docker Compose support. Updated backend configuration to fix database path.

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    DATABASE = os.path.join(BASE_DIR, '..', 'database', 'react-flask-memo.db')
+    DATABASE = os.path.join(BASE_DIR, 'database', 'react-flask-memo.db')
     SECRET_KEY = os.urandom(24)
     SESSION_COOKIE_SAMESITE = "None" 
     SESSION_COOKIE_SECURE = True
